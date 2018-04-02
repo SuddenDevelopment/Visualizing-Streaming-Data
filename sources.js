c8e74a11e95afb73d00910507c0940f1cd84658a
SuddenDevelopment <Anthony.Aragues@Gmail.com>
2018-04-02 14:28:35 -0400

    Update sources.js

--- sources.js
+++ sources.js
@@ -1,1 +1,19 @@
 
+```javascript
+//set stream count
+var intStreams=4;
+//current stream
+var intCurrentStream=1;
+var fnOnMessage=function(objMsg){
+  //assuming this function will send to the stream specified
+  fnSendMessage(intCurrentStream,objMsg);
+  //update the stream pointer
+  if(intCurrentStream === intStreams){
+    //reset
+    intCurrentStream=1;
+  }else{
+    //increment
+    intCurrentStream++;
+  }
+}
+```
